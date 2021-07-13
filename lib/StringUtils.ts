36f4a9ed6e07f91bdfa8556234ff7c842030ce97
yuto-yuto <yuto.nakatsu@gmail.com>
2021-07-13 06:42:47 +0200

    Add min/max indexof

--- lib/StringUtils.ts
+++ lib/StringUtils.ts
@@ -19,6 +19,7 @@ function indexOfBase(
         }
     }
 }
+
 /**
  * Returns the position of the first occurrence of substrings.
  * @param text Text to be searched
@@ -26,43 +27,76 @@ function indexOfBase(
  * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
  */
 export function indexesOf(text: string, searchStrings: string[], position?: number): IndexOfResult | undefined {
-    for (const searchString of searchStrings) {
-        const index = text.indexOf(searchString, position);
-        if (index !== -1) {
-            return {
-                index,
-                foundString: searchString,
-            };
-        }
-    }
+    return indexOfBase(
+        text.indexOf.bind(text),
+        text,
+        searchStrings,
+        position);
 }
+
 /**
- * Returns the position of the first occurrence of substrings.
+ * Returns the last occurrence of a substring in the string.
+ * lastIndexOf is called internally and it returns immediately
+ * when one of searchStrings find it
  * @param text Text to be searched
  * @param searchStrings The substrings to search for in the string
  * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
  */
 export function lastIndexesOf(text: string, searchStrings: string[], position?: number): IndexOfResult | undefined {
-    for (const searchString of searchStrings) {
-        const index = text.lastIndexOf(searchString);
-        if (index !== -1) {
-            return {
-                index,
-                foundString: searchString,
-            };
-        }
+    return indexOfBase(
+        text.lastIndexOf.bind(text),
+        text,
+        searchStrings,
+        position);
+}
+
+function minMaxIndexOfBase(
+    cb: (searchString: string, position?: number) => number,
+    minMax: (...values: number[]) => number,
+    searchStrings: string[],
+    position?: number
+): IndexOfResult | undefined {
+    const results = searchStrings.map((searchString) => {
+        const index = cb(searchString, position);
+        return {
+            index,
+            searchString,
+        };
+    }).filter((x) => x.index !== -1);
+    const min = minMax(...results.map((x) => x.index));
+    const found = results.find((x) => x.index === min);
+    if (found) {
+        return {
+            index: found.index,
+            foundString: found.searchString,
+        };
     }
 }
 
-// const results = searchStrings.map((searchString) => {
-//     const index = text.indexOf(searchString, position);
-//     return {
-//         index,
-//         searchString,
-//     };
-// }).filter((x) => x.index !== -1);
-// const min = Math.min(...results.map((x) => x.index));
-// const found = results.find((x) => x.index === min);
-// if (!found) {
+/**
+ * Return minimum index of the occurrence of substrings
+ * @param text Text to be searched
+ * @param searchStrings The substrings to search for in the string
+ * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+ */
+export function minIndexOf(text: string, searchStrings: string[], position?: number): IndexOfResult | undefined {
+    return minMaxIndexOfBase(
+        text.indexOf.bind(text),
+        Math.min,
+        searchStrings,
+        position);
+}
 
-// }
+/**
+ * Return maximum index of the occurrence of substrings
+ * @param text Text to be searched
+ * @param searchStrings The substrings to search for in the string
+ * @param position The index at which to begin searching the String object. If omitted, search starts at the beginning of the string.
+ */
+export function maxIndexOf(text: string, searchStrings: string[], position?: number): IndexOfResult | undefined {
+    return minMaxIndexOfBase(
+        text.indexOf.bind(text),
+        Math.max,
+        searchStrings,
+        position);
+}
---
+++ test/StringUtils_spec.ts
@@ -0,0 +1,94 @@
+import "mocha";
+import { expect } from "chai";
+import { indexesOf, lastIndexesOf, maxIndexOf, minIndexOf } from "../lib/StringUtils";
+
+describe("StringUtils", () => {
+    describe("indexesOf", () => {
+        const text = "ab bc de bc";
+        it("should return undefined with empty text and searchStrings", () => {
+            const result = indexesOf("", []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined with empty searchStrings", () => {
+            const result = indexesOf(text, []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined when nothing found", () => {
+            const result = indexesOf(text, ["bb", "dd", "aa"]);
+            expect(result).to.be.undefined;
+        });
+        it("should return the result of first occurrence", () => {
+            const result = indexesOf(text, ["bc", "de"]);
+            expect(result).to.deep.equal({
+                index: 3,
+                foundString: "bc",
+            });
+        });
+    });
+    describe("lastIndexesOf", () => {
+        const text = "ab bc de bc";
+        it("should return undefined with empty text and searchStrings", () => {
+            const result = lastIndexesOf("", []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined with empty searchStrings", () => {
+            const result = lastIndexesOf(text, []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined when nothing found", () => {
+            const result = lastIndexesOf(text, ["bb", "dd", "aa"]);
+            expect(result).to.be.undefined;
+        });
+        it("should return the result of first occurrence", () => {
+            const result = lastIndexesOf(text, ["bc", "de"]);
+            expect(result).to.deep.equal({
+                index: 9,
+                foundString: "bc",
+            });
+        });
+    });
+    describe("minIndexOf", () => {
+        const text = "ab bc de bc";
+        it("should return undefined with empty text and searchStrings", () => {
+            const result = minIndexOf("", []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined with empty searchStrings", () => {
+            const result = minIndexOf(text, []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined when nothing found", () => {
+            const result = minIndexOf(text, ["bb", "dd", "aa"]);
+            expect(result).to.be.undefined;
+        });
+        it("should return {index: 0, foundString: 'ab'}", () => {
+            const result = minIndexOf(text, ["de", "ab", "bc"]);
+            expect(result).to.deep.equal({
+                index: 0,
+                foundString: "ab",
+            });
+        });
+    });
+    describe("maxIndexOf", () => {
+        const text = "ab bc de bc";
+        it("should return undefined with empty text and searchStrings", () => {
+            const result = maxIndexOf("", []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined with empty searchStrings", () => {
+            const result = maxIndexOf(text, []);
+            expect(result).to.be.undefined;
+        });
+        it("should return undefined when nothing found", () => {
+            const result = maxIndexOf(text, ["bb", "dd", "aa"]);
+            expect(result).to.be.undefined;
+        });
+        it("should return {index: 6, foundString: 'de'}", () => {
+            const result = maxIndexOf(text, ["bb", "de", "ab"]);
+            expect(result).to.deep.equal({
+                index: 6,
+                foundString: "de",
+            });
+        });
+    });
+});
